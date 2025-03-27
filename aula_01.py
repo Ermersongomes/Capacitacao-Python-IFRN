8af9941ed2e87f448f1eefc06fdce3026514291c
Ermersongomes <ermersonwwe@gmail.com>
2025-03-26 21:19:30 -0300

    Create aula_01.py

---
+++ aula_01.py
@@ -0,0 +1,52 @@
+print ("Olá mundo!")
+
+idade = 23
+nota = 8.5
+nome = "Ermerson Gomes"
+instituto = 'IFRN'
+passou = True
+maior = False
+
+print(nome)
+
+print("A idade é:", idade)
+
+# comentário em uma linha
+
+'''
+Comentário 
+em várias
+linhas
+'''
+
+"""
+Comentário 
+em várias
+linhas
+"""
+
+'''
++ soma
+- subtração
+* multiplicação
+/ divisão
+// divisão de inteiro
+** potenciação
+% resto da divisão
+'''
+
+nota_1 = 7
+nota_2 = 9
+
+soma = nota_1 + nota_2
+print("O resultado da soma é ", soma)
+
+media = (nota_1 + nota_2) / 2
+print("A média é ", media)
+
+print(10+3)
+
+print(10/3) # entrega um numero com casa decimal
+print(10//3) # Mostra apenas o número inteiro sem a casa decimal
+
+print(4*3)
